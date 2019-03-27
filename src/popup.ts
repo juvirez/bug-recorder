736e591f84ff6672ecb13cf995de0cd31231f5af
Aleksandr Semyonov <juvirez@gmail.com>
2019-03-27 15:24:05 +0400

    tslint + prettier

--- src/popup.ts
+++ src/popup.ts
@@ -1,17 +1,18 @@
-let startButton = document.getElementById('start')
-let stopButton = document.getElementById('stop')
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
 
 if (startButton && stopButton) {
-	startButton.addEventListener('click', () => {
-		chrome.tabs.query({active: true, currentWindow: true}, (tabs: chrome.tabs.Tab[]) => {
-			let currentTab = tabs.pop();
-			if (currentTab != null) {
-				chrome.runtime.sendMessage({action: 'start', tab: currentTab});
-			}
-		  })
-	})
+  startButton.addEventListener("click", () => {
+    const tabQueryInfo = { active: true, currentWindow: true };
+    chrome.tabs.query(tabQueryInfo, (tabs: chrome.tabs.Tab[]) => {
+      const currentTab = tabs.pop();
+      if (currentTab != null) {
+        chrome.runtime.sendMessage({ action: "start", tab: currentTab });
+      }
+    });
+  });
 
-	stopButton.addEventListener('click', () => {
-		chrome.runtime.sendMessage({action: 'stop'})
-	})
+  stopButton.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "stop" });
+  });
 }
